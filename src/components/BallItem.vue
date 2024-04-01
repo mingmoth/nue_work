9b6ae07617668eea933cf85939408625ed4607f3
mingmoth <mingmoth@gmail.com>
2024-04-01 22:11:55 +0800

    feat: add BallItem 元件

---
+++ src/components/BallItem.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="ball-item">
+        <slot />
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.ball-item {
+    width: 30px;
+    height: 30px;
+    background-color: #A5F12B;
+    opacity: 1;
+    border-radius: 50%;
+    text-align: center;
+    line-height: 30px;
+    margin: 0 auto;
+}
+</style>
