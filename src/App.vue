1364bddcccb221e65d4da567b9352d4711772aa6
mingmoth <mingmoth@gmail.com>
2024-04-01 22:17:22 +0800

    feat: 在 App.vue 組合 Header & Blocks 元件

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,77 @@
 <script setup>
+import BallWithMovement from './components/BallWithMovement.vue';
+import Blocks from './components/Blocks.vue'
+import Header from './components/Header.vue'
+
+const blocks = [
+    {
+        id: '1',
+        flash: false,
+        ball: true
+    },
+    {
+        id: '2',
+        flash: false,
+        ball: false
+    },
+    {
+        id: '3',
+        flash: true,
+        ball: true
+    },
+    {
+        id: '4',
+        flash: false,
+        ball: false
+    },
+    {
+        id: '5',
+        flash: true,
+        ball: false
+    },
+    {
+        id: '6',
+        flash: false,
+        ball: false
+    },
+    {
+        id: '7',
+        flash: false,
+        ball: true
+    },
+    {
+        id: '8',
+        flash: false,
+        ball: false
+    },
+    {
+        id: '9',
+        flash: true,
+        ball: true
+    },
+]
+
+const centerPosition = [window.innerWidth / 2, window.innerHeight / 2]
+
 </script>
 
 <template>
+    <Header />
+    <div class="app-body">
+        <Blocks :blocks="blocks">
+            <template #block="{ block }">
+                <component :is="block.ball ? BallWithMovement : null" :target-position="centerPosition" :key="block.id">
+                    0
+                </component>
+            </template>
+        </Blocks>
+    </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+.app-body {
+    display: flex;
+    place-items: center;
+    background-color: lightgray;
+}
 </style>
