e755a538490fdc522649afaf8bc53119b0b734c2
mingmoth <mingmoth@gmail.com>
2024-04-01 22:16:29 +0800

    feat: add Header 元件

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<script setup>
+</script>
+
+<template>
+    <div class="header">
+        <button class="header-btn">Open</button>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.header {
+    width: 100vw;
+    display: flex;
+    justify-content: end;
+    padding: 4px;
+    box-sizing: border-box;
+
+    &-btn {
+        border: 1px solid black;
+        cursor: pointer;
+    }
+}
+</style>
